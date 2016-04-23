2d0f55a4d0e2c7f305cfaaf2aa03e3dd53862411
withs_000 <withs_000@RITVIK-LAPTOP>
2016-04-23 10:40:31 -0500

    Add project files.

---
+++ WebApplication1/Contact.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Title="Contact" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.vb" Inherits="WebApplication1.Contact" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>
+
+    <script src="Scripts/jquery-1.4.1.js" type="text/javascript"></script>
+    <script src="Scripts/jquery-1.4.1.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        var menufooter = null;
+        var splitterget = null;
+        var splitterfind = null;
+        var pnlDownPaneget = null;
+        var pnlLeftPaneget = null;
+        var homepage = null;
+      
+        
+        
+
+        function fn_BlockUI() {
+            $.blockUI({
+                message: '<img src="AjaxProgress.gif"/>',
+                css: {}, overlayCSS: { backgroundColor: '#FFFFFF', opacity: 0.3 }
+
+            });
+
+        }
+
+       
+ 
+        function unblockUIscript() {
+            $.unblockUI();
+        }
+
+        
+         
+
+    </script>
+        
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Conditional">
+
+        <ContentTemplate>
+
+           
+
+        <table style="width: 100%; border: 1px solid #5BC0DE; background-color: #EBCCCC">
+            <tr>
+                <td>sdfsdfsdf</td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>dfhfdhdfh</td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" Text="Button" />
+                    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+                </td>
+            </tr>
+        </table>
+
+              </ContentTemplate>
+
+        <Triggers>
+        </Triggers>
+
+    </asp:UpdatePanel>
+
+    </h2>
+    <p>Your contact page.</p>
+
+    <address>
+        One Microsoft Way<br />
+        Redmond, WA 98052-6399<br />
+        <abbr title="Phone">P:</abbr>
+        425.555.0100
+    </address>
+
+    <address>
+        <strong>Support:</strong><a href="mailto:Support@example.com">Support@example.com</a><br />
+        <strong>Marketing:</strong><a href="mailto:Marketing@example.com">Marketing@example.com</a>
+    </address>
+</asp:Content>
---
+++ WebApplication1/Contact.aspx.vb
@@ -0,0 +1,81 @@
+﻿Public Class Contact
+    Inherits Page
+    Public Property nopostcount() As Integer
+        Get
+
+
+            Return DirectCast(ViewState("nopostcount"), Integer)
+        End Get
+        Set(ByVal value As Integer)
+            ViewState("nopostcount") = value
+        End Set
+    End Property
+
+    Public Property btcount() As Integer
+        Get
+
+            Return DirectCast(ViewState("btcount"), Integer)
+        End Get
+        Set(ByVal value As Integer)
+            ViewState("btcount") = value
+        End Set
+    End Property
+
+    Public Property pagecount() As Integer
+        Get
+
+            Return DirectCast(ViewState("pagecount"), Integer)
+        End Get
+        Set(ByVal value As Integer)
+            ViewState("pagecount") = value
+        End Set
+    End Property
+    Public Property postcount() As Integer
+        Get
+
+            Return DirectCast(ViewState("postcount"), Integer)
+        End Get
+        Set(ByVal value As Integer)
+            ViewState("postcount") = value
+        End Set
+    End Property
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        If IsPostBack Then
+            postcount = postcount + 1
+        Else
+            pagecount() = 0
+            postcount() = 0
+            nopostcount() = 0
+            btcount() = 0
+            nopostcount = nopostcount + 1
+        End If
+        pagecount = pagecount + 1
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        btcount = btcount + 1
+
+        Dim str As String
+        str = String.Concat("Page loaded times : " + pagecount.ToString _
+                        + "<br/>" + "Page post backed times " + postcount.ToString _
+                        + "<br/>" + "Page not post backed times" + nopostcount.ToString _
+                        + "<br/>" + "Button clicked times" + btcount.ToString + "<br/>"
+                             )
+
+        'Page.Response.Write(str.ToString + "<br/>" + TextBox1.Text.ToString
+        '    )
+        Me.Label1.Text = "fdgbdfmbjkfdb" + Environment.NewLine + TextBox1.Text.ToString
+
+        DisplayErrorMessage(TextBox1.Text.ToString)
+    End Sub
+
+
+    Private Sub DisplayErrorMessage(ByVal errMsg As String)
+        If Not String.IsNullOrEmpty(errMsg) And Not String.IsNullOrWhiteSpace(errMsg) Then
+            ScriptManager.RegisterStartupScript(Me, Me.GetType(), "Message", String.Concat("alert(""", errMsg.Replace("""", "'"), """);"), True)
+        End If
+    End Sub
+
+End Class
